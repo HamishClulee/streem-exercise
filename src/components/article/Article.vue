d8122bff571116a1baf85e60ef438ce2078325b0
HamishClulee <hamish.clulee@hotmail.com>
2021-08-27 11:29:53 +1200

    Split out ArticleExcerpts as seperate component

--- src/components/article/Article.vue
+++ src/components/article/Article.vue
@@ -14,51 +14,23 @@
       :word-count="json.word_count"
     />
 
-    <h4 class="text-lg my-4 text-streem-dark-blue border-b border-streem-dark-blue w-full">
-      Excerpts
-    </h4>
-    <!-- eslint-disable vue/no-v-html -->
-    <!-- DOMPurify in usage -->
-    <div
-      v-for="(excerpt, index) in json.excerpts"
-      :key="index"
-      class="my-4"
-    >
-      <span>
-        <h5>
-          Keywords: <em
-            v-for="keyword in excerpt.keywords"
-            :key="keyword"
-            class="mr-2"
-          >{{ keyword }}</em>
-        </h5>
-      </span>
-      <button
-        class="mb-2 text-left"
-        v-html="santise(excerpt.text)"
-      />
-      <!--eslint-enable-->
-    </div>
+    <ArticleExcerpts :excerpts="json.excerpts" />
   </article>
 </template>
 <script>
-import DOMPurify from 'dompurify'
 import ArticleDetails from './ArticleDetails.vue'
+import ArticleExcerpts from './ArticleExcerpts.vue'
 
 export default {
   name: 'Article',
   components: {
     ArticleDetails,
+    ArticleExcerpts,
   },
   props: {
     json: {
       type: Object,
       default: null,
-    },
-  },
-  methods: {
-    santise(html) {
-      return DOMPurify.sanitize(html)
     },
   },
 }
---
+++ src/components/article/ArticleExcerpts.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <h4 class="text-lg my-4 text-streem-dark-blue border-b border-streem-dark-blue w-full">
+      Excerpts
+    </h4>
+    <!-- eslint-disable vue/no-v-html -->
+    <!-- DOMPurify in usage -->
+    <div
+      v-for="(excerpt, index) in excerpts"
+      :key="index"
+      class="my-4"
+    >
+      <span>
+        <h5>
+          Keywords: <em
+            v-for="keyword in excerpt.keywords"
+            :key="keyword"
+            class="mr-2"
+          >{{ keyword }}</em>
+        </h5>
+      </span>
+      <button
+        class="mb-2 text-left"
+        v-html="santise(excerpt.text)"
+      />
+      <!--eslint-enable-->
+    </div>
+  </section>
+</template>
+<script>
+import DOMPurify from 'dompurify'
+export default {
+  name: 'ArticleExcerpts',
+  props: {
+    excerpts: {
+      type: Array,
+      default: null,
+      required: true,
+    },
+  },
+  methods: {
+    santise(html) {
+      return DOMPurify.sanitize(html)
+    },
+  },
+}
+</script>
+<style>
+</style>
