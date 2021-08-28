f82531da8f9227847b9e2471df934d6e0c4e50c6
HamishClulee <hamish.clulee@hotmail.com>
2021-08-28 14:21:52 +1200

    Improve details sections, check img src, hide empty items, styling

--- src/api/index.js
+++ src/api/index.js
@@ -3,28 +3,21 @@ import axios from 'axios'
 export default class Api {
   API_URL =
     'https://frontend-engineering-test-api.netlify.app/.netlify/functions/articles'
-  axios
+  ax
   numArticlesPerPage = 10
   lastPageNumRequested = 0
+  // currentPageNumber ???
 
   constructor() {
-    this.axios = axios.create({
+    this.ax = axios.create({
       baseURL: this.API_URL
     })
-  }
-
-  incrementPageNumber() {
-    this.currentPage++
-  }
-
-  getCurrentPageNumber() {
-    return this.currentPage
   }
 
   getNextPageOfArticles(currentPage) {
     if (currentPage > this.lastPageNumRequested) {
       this.lastPageNumRequested = currentPage
-      return this.axios.get(
+      return this.ax.get(
         `?page=${currentPage}&pageSize=${this.numArticlesPerPage}`
       )
     }
--- src/components/article/ArticleDetails.vue
+++ src/components/article/ArticleDetails.vue
@@ -1,19 +1,34 @@
 <template>
-  <details
-    open
-    class="text-sm util-500 mb-2"
-  >
-    <summary>Published by {{ author.name }}</summary>
-    <summary class="ml-8">
-      Word count: {{ wordCount }}
-    </summary>
-    <summary class="ml-8">
-      Published at: {{ new Date(createdAt) }}
-    </summary>
-    <summary class="ml-8">
-      Published by: {{ source.name }} <img :src="source.logo_url">
-    </summary>
-  </details>
+  <section class="flex flex-row items-center justify-between">
+    <details
+      open
+      class="text-sm util-500 mb-2"
+    >
+      <summary>Published by {{ source.name }}</summary>
+      <summary class="ml-8">
+        Word count: {{ wordCount }}
+      </summary>
+      <summary class="ml-8">
+        Published at: {{ new Date(createdAt) }}
+      </summary>
+      <summary
+        v-if="author.name"
+        class="ml-8 flex flex-row items-center"
+      >
+        Authored by: {{ author.name }}
+      </summary>
+    </details>
+    <div
+      v-if="safeSrc"
+      class="mr-8 ml-4"
+    >
+      <img
+        class="w-32"
+        :src="safeSrc"
+      >
+    </div>
+    <section />
+  </section>
 </template>
 
 <script>
@@ -39,6 +54,25 @@ export default {
       type: Object,
       default: null,
       required: true,
+    },
+  },
+  data() {
+    return {
+      safeSrc: null,
+    }
+  },
+  mounted() {
+    this.checkSrc()
+    console.log(this.safeSrc)
+  },
+  methods: {
+    checkSrc() {
+      var img = new Image()
+      img.onload = this.imgOk
+      img.src = this.source.logo_url
+    },
+    imgOk() {
+      this.safeSrc = this.source.logo_url
     },
   },
 }
